6f6cb912b0a499cd43751c11981465b40e412c61
Sergey Grimanov <tosergi@gmail.com>
2019-06-28 23:08:50 +0300

    создан класс MainWindow создан класс application.properties

--- src/main/java/View/MainWindow.java
+++ src/main/java/View/MainWindow.java
@@ -1,11 +1,28 @@
 package View;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
 import javax.swing.JFrame;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MainWindow extends JFrame {
-String sssssss;
+
+public MainWindow(@Value("${width}") int width,
+    @Value("${height}") int height,
+    @Value("${title}") String title){
+
+  Canvas content = new Canvas();
+  Dimension size = new Dimension(width, height);
+  content.setPreferredSize(size);
+  setResizable(false);
+  setTitle(title);
+  getContentPane().add(content);
+  pack();
+  setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+}
 
 
 }
