64438abd1e85faa319ec4cc71792aadcfe2481e4
Sergey Grimanov <tosergi@gmail.com>
2019-06-29 23:23:09 +0300

    расширен класс MainWindow

--- src/main/java/View/MainWindow.java
+++ src/main/java/View/MainWindow.java
@@ -1,7 +1,11 @@
-package View;
 
+package View;
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 import javax.swing.JFrame;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,9 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class MainWindow extends JFrame {
 
+  private BufferedImage buffer;
+  private int[] bufferData;
+  private Graphics bufferGraphics;
+  private int clearColor;
+
 public MainWindow(@Value("${width}") int width,
     @Value("${height}") int height,
-    @Value("${title}") String title){
+    @Value("${title}") String title, int _clearColor){
 
   Canvas content = new Canvas();
   Dimension size = new Dimension(width, height);
@@ -20,7 +29,23 @@ public MainWindow(@Value("${width}") int width,
   setTitle(title);
   getContentPane().add(content);
   pack();
+
   setDefaultCloseOperation(EXIT_ON_CLOSE);
+  buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+  bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+  bufferGraphics = buffer.getGraphics();
+  clearColor = _clearColor
+}
+
+public void clear(){
+  Arrays.fill(bufferData, clearColor);
+}
+
+public void render(){
+
+}
+
+public void swapBuffers(){
 
 }
 
