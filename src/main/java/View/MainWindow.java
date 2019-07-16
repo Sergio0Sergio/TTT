96a63e2aa2a55b494232633c6079d049b7e996b4
Sergey Grimanov <tosergi@gmail.com>
2019-07-16 22:57:54 +0300

    да нифига.

--- src/main/java/View/MainWindow.java
+++ src/main/java/View/MainWindow.java
@@ -3,6 +3,7 @@ package View;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Arrays;
@@ -12,17 +13,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MainWindow extends JFrame {
-
+  private Canvas content;
   private BufferedImage buffer;
   private int[] bufferData;
   private Graphics bufferGraphics;
   private int clearColor;
 
+  private BufferStrategy bufferStrategy;
+
 public MainWindow(@Value("${width}") int width,
     @Value("${height}") int height,
-    @Value("${title}") String title, int _clearColor){
+    @Value("${title}") String title, int _clearColor, int numBuffers){
 
-  Canvas content = new Canvas();
+  content = new Canvas();
   Dimension size = new Dimension(width, height);
   content.setPreferredSize(size);
   setResizable(false);
@@ -35,6 +38,8 @@ public MainWindow(@Value("${width}") int width,
   bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
   bufferGraphics = buffer.getGraphics();
   clearColor = _clearColor;
+  content.createBufferStrategy(numBuffers);
+  bufferStrategy = content.getBufferStrategy();
 }
 
 public void clear(){
@@ -47,7 +52,9 @@ g.drawImage(buffer, 0,0, null);
 }
 
 public void swapBuffers(){
-
+  Graphics g = bufferStrategy.getDrawGraphics();
+  g.drawImage(buffer, 0, 0, null);
+  bufferStrategy.show();
 }
 
 
